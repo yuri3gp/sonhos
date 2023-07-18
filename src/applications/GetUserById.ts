5eb0300224ad2d7e8fb3ad2c9b7e7dcd9a869a80
Yuri Rodrigues Teixeira <71528635+yuri3gp@users.noreply.github.com>
2023-07-18 14:58:35 -0300

    teste

--- src/applications/GetUserById.ts
+++ src/applications/GetUserById.ts
@@ -1,11 +1,19 @@
+import { User } from "../domain/entities/User";
 import { IUserRepository } from "../domain/data/repositories/IUserRepository";
 
 type input = { id: number };
 
 export class GetUserById {
-	constructor(private readonly user_repository ?: IUserRepository) {}
-	
-	async execute({ id }: input): Promise<any> {
+	constructor(private readonly user_repository?: IUserRepository) { }
 
+	async execute({ id }: input): Promise<any> {
+		const user = new User(
+			1,
+			"joaquim",
+			"joaquim4512@gmail.com",
+			"8489df8454zsdfasef",
+			Date.now().toLocaleString()
+		)
+		this.user_repository?.createUser(user)
 	}
 }
