798f444231eae3fe28dfd7e0f8b57273a7d2669b
Yuri Rodrigues Teixeira <71528635+yuri3gp@users.noreply.github.com>
2023-07-27 22:22:20 -0300

    api ready

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,11 @@ import { UserRoutes } from './infra/routes/UserRoutes.ts';
 import { IHttpServerAdapter } from './infra/http/IHttpServerAdapter.ts';
 import { ReadyController } from './infra/controller/ReadyController.ts';
 import { UserController } from './infra/controller/UserController.ts';
+import { PrismaConnection } from './infra/database/PrismaConnection.ts';
+import UserRepositoryPrisma from './infra/database/repository/UserRepositoryPrisma.ts';
+
+const connection = new PrismaConnection().createConnection()
+const userRepository = new UserRepositoryPrisma(connection)
 
 const http_server: IHttpServerAdapter = new ExpressHttpServer();
 new ReadyController(http_server);
@@ -12,3 +17,4 @@ const userController = new UserController()
 new UserRoutes(http_server, userController)
 
 http_server.startServerHttp(PORT);
+
