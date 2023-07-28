c7968105863daee9ea50db5fdc462797805194a5
Yuri Rodrigues Teixeira <71528635+yuri3gp@users.noreply.github.com>
2023-07-27 22:10:31 -0300

    env

--- src/infra/config/environment.ts
+++ src/infra/config/environment.ts
@@ -7,3 +7,8 @@ if (dotenv) {
 export const DATABASE_URL = process.env.DATABASE_URL || 'development';
 export const PORT = Number(process.env.PORT) || 3000
 export const ENV = process.env.ENV
+export const DB_USERNAME = process.env.DB_USERNAME || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || '3306';
+export const DB_NAME = process.env.DB_NAME || 'dream';
